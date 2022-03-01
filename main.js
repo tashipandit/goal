fbd71e64525ef5d55c3b97de081b33d0d72667b8
tashipandit <93296077+tashipandit@users.noreply.github.com>
2022-03-01 17:23:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+
+// Create canvas variable
+var canvas= new fabric.Canvas('myCanvas');
+//Set initial positions for ball and hole images.
+ball_y=0;
+ball_y=0;
+ball_y=400;
+ball_y=800;
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	// write code to Upload golf image on the canvas
+	new_image();
+}
+
+function new_image()
+{
+	// write code to Upload ball image on canvas
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	if((ball_x==hole_x)&&(ball_y==hole_y))
+	{
+		 canvas.remove(ball_obj);
+		  console.log("You have Hit the Goal!!!");
+		   document.getElementById("hd3").innerHTML="You have Hit the Goal!!!";
+		    document.getElementById("myCanvas").style.borderColor="red";
+	}
+	else {
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+
+
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
+
